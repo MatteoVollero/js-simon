a8513f1ff2c858160285d6d5214c4360dfd6596c
MatteoVollero <m.vollero120796@gmail.com>
2020-09-04 16:59:50 +0200

    Aggiunta controlli

--- js/main.js
+++ js/main.js
@@ -9,12 +9,16 @@ $(document).ready(
     var numeriInseriti = [];
     var numeriIndovinati = [];
     var punteggio  = 0;
+    var numero;
     var finito = false;
     var stampato = false;
 
     //Espongo i numeri tramite un alert
     for(var i = 0 ; i < 5 ; i ++){
-      var numero = Math.floor(Math.random() * 5 + 1);
+      do{//Con questo ciclo evito ripetizioni
+        numero = Math.floor(Math.random() * 100 + 1);
+      }
+      while(isInArray(numeriCasuali,numero) == true)
       numeriCasuali.push(numero);
       giaPresente.push(false);
       alert(numero);
@@ -26,7 +30,17 @@ $(document).ready(
         alert("Bene ora vediamo quanti numeri ricordi... hai 5 tentativi");
 
         for(var i = 0 ;i < 5 ;i ++){
-          numeriInseriti.push(parseInt(prompt("Inserisci un numero: ")));
+          numero = parseInt(prompt("Inserisci un numero: "));
+          while(isInArray(numeriInseriti,numero) == true){
+            numero = parseInt(prompt("Numero gia inserito inseriscine un altro: "));
+          }
+          while(numero < 1){
+            numero = parseInt(prompt("Il numero inserito è troppo piccolo inserirne un altro (range [1,100]): "));
+          }
+          while(numero > 100){
+            numero = parseInt(prompt("Il numero inserito è troppo grande inserirne un altro (range [1,100]): "));
+          }
+          numeriInseriti.push(numero);
           numeroIndovinato()
         }
 
@@ -53,5 +67,19 @@ $(document).ready(
             console.log("Punteggio ---> " + punteggio);
             stampato = true;
         },2000);
+
+
+        function isInArray(numeri,numero){
+
+          for(var i = 0 ; i < numeri.length ; i ++){
+            if(numeri.indexOf(numero) != -1){
+              console.log("numero presente ---> " + numero);
+              return true; //Vuole dire che il numero è presente
+            } else {
+              console.log("numero assente ---> " + numero);
+              return false;//Vuole dire che non è presente
+            }
+          }
+        }
 
 });
